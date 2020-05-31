7f1ca7c8cd2585e52d140aeacfb00051ec4e0c08
mp <mploureno@gmail.com>
2020-05-31 20:46:15 -0300

    add a few actions to client controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,30 @@
 class ClientsController < ApplicationController
+  
+  def index
+  end 
+
+  def new 
+    @client = current_user.clients.build
+  end
+  
+  def create 
+    @client = current_user.clients.new(client_params)
+    if @client.save
+      redirect_to root_path
+    else
+      render 'new'
+    end    
+  end
+
+  private
+    
+    def client_params
+      params.require(:client).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :mobile_phone
+        :profile_photo      
+      )
+
 end
