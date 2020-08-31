ea0ad4e8b8a3a44177662deca7ffef53360a908d
mp <mploureno@gmail.com>
2020-08-31 19:23:39 -0300

    Tell router to use users/registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :clients
-  
+
   root to: 'clients#index'
 end
-	
-
-
-
