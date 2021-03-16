bf326b0f27d24a69fcf47e09e0bf11109813f7d1
mp <mploureno@gmail.com>
2021-03-16 17:06:14 -0300

    extract media queries to a mixin/ small fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def load_svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |svg|
+    File.open("app/assets/images/svg/#{path}", "rb") do |svg|
       raw svg.read
     end
   end
